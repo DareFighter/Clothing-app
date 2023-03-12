4836c09a5e808426d0bbddca0e89772b6b1a7aed
Shreyas Kumar R <shreyas.kumar@payhuddle.com>
2023-03-12 19:56:22 +0530

    added documents to firebase

--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -1,12 +1,19 @@
-import { createContext, useState } from "react";
-import PRODUCTS from "../shop-data.json";
+import { createContext, useEffect, useState } from "react";
+
+import { getCategoriesAndDocuments } from "../utils/firebase.utils";
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+
+  useEffect(async () => {
+    const categoryMap = await getCategoriesAndDocuments("categories");
+    console.log(categoryMap);
+  }, []);
+
   const value = { products };
 
   return (
